fd31468034a5de0bd6986ad106ce9b0d3758cb63
Nickolay <49285714+nickolyapop@users.noreply.github.com>
2023-08-27 20:12:28 +0300

    Create 3_1.js
    Задание 1 модуль C3.3. Прототип и конструктор объекта
    
    Написать, функцию, которая принимает в качестве аргумента объект и выводит в консоль все ключи и значения только собственных свойств. Данная функция не должна возвращать значение.

---
+++ 3_1.js
@@ -0,0 +1,19 @@
+// Написать, функцию, которая принимает в качестве аргумента объект 
+// и выводит в консоль все ключи и значения только собственных свойств. 
+// Данная функция не должна возвращать значение.
+
+function printObject (obj) {
+  for (let prop in obj) {
+    if (obj.hasOwnProperty(prop)) {
+      console.log(prop, obj[prop]);
+    }
+  }
+}
+
+const person ={
+  city: "Cheboksary"
+}
+const student = Object.create(person);
+student.ownCity = "Marpos";
+
+printObject(student);
